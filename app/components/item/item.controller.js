9147985b5adc12b1429010d0e9b907de25ebf0d8
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2016-11-06 15:27:38 +0300

    Editing of a shop

--- app/components/item/item.controller.js
+++ app/components/item/item.controller.js
@@ -1,7 +1,7 @@
 angular.module("item").controller("ItemController", ["$scope", ($scope) => {
     var list = $scope.$parent.$parent.$parent.list;
 
-    $scope.bodyUrl = "components/item/item-body-" + $scope.type + ".html";
+    if (!$scope.bodyUrl) $scope.bodyUrl = "components/item/item-body-" + $scope.type + ".html";
 
     $scope.parentId = "#" + $scope.parentId;
     $scope.innerListVisible = false;
@@ -19,12 +19,20 @@ angular.module("item").controller("ItemController", ["$scope", ($scope) => {
         switch ($scope.bodyUrl) {
             case "components/item/item-body-collapsable.html":
                 $scope.bodyUrl = "components/item/item-body-collapsable-edit.html";
+                $scope.editedItem = {
+                    name: $scope.item.name,
+                    data: {}
+                };
+                for (var data in $scope.item.data) {
+                    $scope.editedItem.data[data] = $scope.item.data[data];
+                };
                 break;
             case "components/item/item-body-collapsable-edit.html":
                 $scope.bodyUrl = "components/item/item-body-collapsable.html";
                 break;
             case "components/item/item-body-table.html":
                 $scope.bodyUrl = "components/item/item-body-table-edit.html";
+                $scope.editedItem = {};
                 break;
             case "components/item/item-body-table-edit.html":
                 $scope.bodyUrl = "components/item/item-body-table.html";
@@ -34,5 +42,14 @@ angular.module("item").controller("ItemController", ["$scope", ($scope) => {
 
     $scope.closeAll = function () {
         $(".collapse").collapse("hide");
-    }
+    };
+
+    $scope.saveItem = function () {
+        $scope.item.name = $scope.editedItem.name;
+        $scope.item.data = $scope.editedItem.data;
+
+        $scope.editedItem = {};
+
+        $scope.toggleEditMode();
+    };
 }]);
